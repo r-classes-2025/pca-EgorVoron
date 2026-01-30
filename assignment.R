bdd3ea5e02eb509c8059a2ae0cb5b128584ac958
EgorVoron <33517421+EgorVoron@users.noreply.github.com>
2026-01-30 19:57:17 +0300

    undo

--- assignment.R
+++ assignment.R
@@ -8,64 +8,42 @@ library(factoextra)
 # 1. отберите 6 главных персонажей (по количеству реплик)
 # сохраните как символьный вектор
 top_speakers <- friends |> 
-  count(speaker, sort = TRUE) |> 
-  slice_max(n, n = 6) |> 
-  pull(speaker)
+  # ваш код здесь
   
 # 2. отфильтруйте топ-спикеров, 
 # токенизируйте их реплики, удалите из них цифры
 # столбец с токенами должен называться word
 # оставьте только столбцы speaker, word
 friends_tokens <- friends |> 
-  filter(speaker %in% top_speakers) |>
-  unnest_tokens(word, text) |>
-  filter(!str_detect(word, "\\d")) |>
-  select(speaker, word)
+  # ваш код здесь
 
 # 3. отберите по 500 самых частотных слов для каждого персонажа
 # посчитайте относительные частотности для слов
 friends_tf <- friends_tokens |>
-  count(speaker, word, sort = TRUE) |>
-  group_by(speaker) |>
-  slice_max(n, n = 500, with_ties = FALSE) |>
-  mutate(tf = n / sum(n)) |>
-  ungroup() |>
-  select(speaker, word, tf)
+  # ваш код здесь
 
 # 4. преобразуйте в широкий формат; 
 # столбец c именем спикера превратите в имя ряда, используя подходящую функцию 
 friends_tf_wide <- friends_tf |> 
-  pivot_wider(names_from = word, values_from = tf, values_fill = 0) |>
-  column_to_rownames("speaker")
+  # ваш код здесь
 
 # 5. установите зерно 123
-set.seed(123)
 # проведите кластеризацию k-means (k = 3) на относительных значениях частотности (nstart = 20)
 # используйте scale()
-k <- 3
-km.out <- kmeans(scale(friends_tf_wide), centers = k, nstart = 20)
+
+# ваш код здесь
+km.out <- # ваш код здесь
 
 
 # 6. примените к матрице метод главных компонент (prcomp)
 # центрируйте и стандартизируйте, использовав аргументы функции
-pca_fit <- prcomp(
-  friends_tf_wide, 
-  center = TRUE, 
-  scale. = TRUE
-)
+pca_fit <- # ваш код здесь
 
 # 7. Покажите наблюдения и переменные вместе (биплот)
 # в качестве геома используйте текст (=имя персонажа)
 # цветом закодируйте кластер, выделенный при помощи k-means
 # отберите 20 наиболее значимых переменных (по косинусу, см. документацию к функции)
 # сохраните график как переменную q
-N <- 20
-q <- fviz_pca_biplot(
-  pca_fit, 
-  geom.ind = "text",
-  col.ind = factor(km.out$cluster),
-  repel = TRUE,
-  select.var = list(cos2 = N),
-)
 
-q
+q <- # ваш код здесь
+
